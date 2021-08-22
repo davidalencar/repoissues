eb6a10c860651dab3b7383e24ea939a9edbfbf90
David <davidalencarignacio@gmail.com>
2021-08-22 20:54:27 -0300

    Initial commit

---
+++ lib/repoissues.ex
@@ -0,0 +1,18 @@
+defmodule Repoissues do
+  @moduledoc """
+  Documentation for `Repoissues`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Repoissues.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/repoissues_test.exs
@@ -0,0 +1,8 @@
+defmodule RepoissuesTest do
+  use ExUnit.Case
+  doctest Repoissues
+
+  test "greets the world" do
+    assert Repoissues.hello() == :world
+  end
+end
